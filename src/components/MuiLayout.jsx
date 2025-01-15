b148737e81db4016c6423aa366d269e934423671
Rohit <rohitchikmath69@gmail.com>
2025-01-15 16:44:32 +0530

    Grid

--- src/components/MuiLayout.jsx
+++ src/components/MuiLayout.jsx
@@ -1,31 +1,84 @@
-import { Box } from '@mui/material'
+import { Box, Divider, Grid2, Stack } from '@mui/material'
 
 const MuiLayout = () => {
 	return (
-		<>
-			<Box
-				// component={'span'}
-				sx={{
-					backgroundColor: 'primary.main',
-					color: 'white',
-					height: '100px',
-					width: '100px',
-					padding: '16px',
-					'&:hover': {
-						backgroundColor: 'primary.light',
-					},
-				}}
+		<Stack>
+			<Stack
+				direction={'row'}
+				spacing={2}
+				sx={{ border: '1px solid' }}
+				divider={<Divider orientation='vertical' flexItem />}
+				padding={2}
 			>
-				MuiLayout
-			</Box>
-			<Box
-				display={'flex'}
-				height={'100px'}
-				width={'100px'}
-				p={2}
-				bgcolor={'success.light'}
-			></Box>
-		</>
+				<Box
+					// component={'span'}
+					sx={{
+						backgroundColor: 'primary.main',
+						color: 'white',
+						height: '100px',
+						width: '100px',
+						padding: '16px',
+						'&:hover': {
+							backgroundColor: 'primary.light',
+						},
+					}}
+				>
+					MuiLayout
+				</Box>
+				<Box
+					display={'flex'}
+					height={'100px'}
+					width={'100px'}
+					p={2}
+					bgcolor={'success.light'}
+				></Box>
+			</Stack>
+			<Grid2 container my={4} spacing={2}>
+				<Grid2 size={8}>
+					<Box bgcolor='primary.light' p={2}>
+						Item 1
+					</Box>
+				</Grid2>
+				<Grid2 size={4}>
+					<Box bgcolor='primary.light' p={2}>
+						Item 2
+					</Box>
+				</Grid2>
+				<Grid2 size={4}>
+					<Box bgcolor='primary.light' p={2}>
+						Item 3
+					</Box>
+				</Grid2>
+				<Grid2 size={8}>
+					<Box bgcolor='primary.light' p={2}>
+						Item 4
+					</Box>
+				</Grid2>
+			</Grid2>
+			{/* mutiple breakpoints */}
+			<Grid2 container rowSpacing={1} columnSpacing={2}>
+				<Grid2 size={{ xs: 6, md: 8 }}>
+					<Box bgcolor='primary.light' p={2}>
+						xs=6 md=8
+					</Box>
+				</Grid2>
+				<Grid2 size={{ xs: 6, md: 4 }}>
+					<Box bgcolor='primary.light' p={2}>
+						xs=6 md=4
+					</Box>
+				</Grid2>
+				<Grid2 size={{ xs: 6, md: 4 }}>
+					<Box bgcolor='primary.light' p={2}>
+						xs=6 md=4
+					</Box>
+				</Grid2>
+				<Grid2 size={{ xs: 6, md: 8 }}>
+					<Box bgcolor='primary.light' p={2}>
+						xs=6 md=8
+					</Box>
+				</Grid2>
+			</Grid2>
+		</Stack>
 	)
 }
 
